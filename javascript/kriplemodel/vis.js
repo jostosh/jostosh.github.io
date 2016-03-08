bad83b58b5878db731713187e392f13462e267c7
¨Diederik <diederikgreveling@gmail.com>
2016-03-08 12:42:41 +0100

    Testing commit removed

--- javascript/kriplemodel/vis.js
+++ javascript/kriplemodel/vis.js
@@ -25,8 +25,6 @@
 
 "use strict";
 
-/* test commmit */
-
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
