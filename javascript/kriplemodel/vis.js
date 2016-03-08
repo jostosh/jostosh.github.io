06dee5430fb86066a7789c6ce59dea72d0187094
¨Diederik <diederikgreveling@gmail.com>
2016-03-08 12:37:20 +0100

    Testing commit

--- javascript/kriplemodel/vis.js
+++ javascript/kriplemodel/vis.js
@@ -25,6 +25,8 @@
 
 "use strict";
 
+/* test commmit */
+
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
