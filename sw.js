f6c5b5702aab4e4562a545857fb2151fb9f31d30
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2019-07-10 00:41:31 +0200

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ad8b9e1a04a91edbdad4.js"
+    "url": "webpack-runtime-3db4ce72974bcb43c49f.js"
   },
   {
     "url": "styles.54ed04fb82838814d1dd.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "07fdf1bfebb8daed0225b7d4eff58868"
+    "revision": "6be9241a1fd18496c48d2abec8abddee"
   },
   {
     "url": "component---src-pages-404-js-5b2eb035c8af605caf4c.js"
